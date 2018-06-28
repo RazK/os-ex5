1bda131bcc6ba11fa56fb3cb8ce964bf8455ce86
Raz <raz@yosigal.com>
2018-06-28 20:24:35 +0300

    * Resumed school constants, something is wrong and the simple test fails. Debug this later. * Don't forget to clean up testing shit and resume original school files for real testing

--- SimpleTest.cpp
+++ SimpleTest.cpp
@@ -52,9 +52,27 @@ void test_3(){
     printf("READ VALUE : %d", val);
 }
 
+int test_simple()
+{
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+        printf("writing to %llu\n", (long long int) i);
+        VMwrite(5 * i * PAGE_SIZE, i);
+    }
+
+    for (uint64_t i = 0; i < (2 * NUM_FRAMES); ++i) {
+        word_t value;
+        VMread(5 * i * PAGE_SIZE, &value);
+        printf("reading from %llu %d\n", (long long int) i, value);
+        assert(uint64_t(value) == i);
+    }
+    printf("success\n");
+
+    return 0;
+}
+
 int main(int argc, char **argv) {
     VMinitialize();
-    test_3();
+    test_simple();
 }
 
 /*
